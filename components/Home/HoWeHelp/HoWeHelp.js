131ba21d39bfc199cbc01240303af8ca00be389a
Md. Hazrat Ali <md.hazratali4510@gmail.com>
2022-10-18 01:36:07 +0600

    banner fully done and new section ready to made

---
+++ components/Home/HoWeHelp/HoWeHelp.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Icon1 from "/public/img/Home/icons/Icon5.svg"
+import Icon2 from "/public/img/Home/icons/Icon6.svg"
+import Icon3 from "/public/img/Home/icons/Icon7.svg"
+import Icon4 from "/public/img/Home/icons/Icon8.svg"
+import Icon5 from "/public/img/Home/icons/Icon9.svg"
+import Icon6 from "/public/img/Home/icons/Icon10.svg"
+
+const HoWeHelp = () => {
+    const data = [
+        {
+            title: "Urgent Care",
+            icon: Icon1
+        },
+        {
+            title: "Surgery",
+            icon: Icon2
+        },
+        {
+            title: "Checkup",
+            icon: Icon3
+        },
+        {
+            title: "Dental",
+            icon: Icon4
+        },
+        {
+            title: "Spaying",
+            icon: Icon5
+        },
+        {
+            title: "Neutering",
+            icon: Icon6
+        },
+    ]
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default HoWeHelp;
