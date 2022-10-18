72306fe83b98e0e463b90b59680a7bcc3366c5ad
Md. Hazrat Ali <md.hazratali4510@gmail.com>
2022-10-18 18:10:21 +0600

    How we help section completed

--- components/Home/HoWeHelp/HoWeHelp.js
+++ components/Home/HoWeHelp/HoWeHelp.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, Component } from 'react';
 import Icon1 from "/public/img/Home/icons/Icon5.svg"
 import Icon2 from "/public/img/Home/icons/Icon6.svg"
 import Icon3 from "/public/img/Home/icons/Icon7.svg"
@@ -9,6 +9,9 @@ import Styles from './HoWeHelp.module.css'
 import Image from 'next/image';
 import UnderTitle from "/public/img/under-title.svg"
 import UnderTitleMobile from "/public/img/under-title-mobile.svg"
+import Slider from "react-slick";
+
+
 
 const HoWeHelp = () => {
 
@@ -46,17 +49,52 @@ const HoWeHelp = () => {
             icon: Icon6
         },
     ]
+
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 1500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        cssEase: "linear",
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        swipeToSlide: false,
+        touchMove: false,
+        pauseOnHover: false,
+        responsive: [
+
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 3,
+                },
+            },
+            {
+                breakpoint: 400,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+        ],
+    };
+
+
+
+
     return (
         <section id={Styles.HowWeHelp}>
             <div className={`container-fluid ${Styles.container}`}>
                 <div className="d-flex justify-content-center flex-column align-items-center">
                     <h2 className='title text-center'>How We Help</h2>
-                    <Image className='img-fluid' src={deviceWidth < "767" ? UnderTitleMobile : UnderTitle} alt='' />
+                    <Image src={deviceWidth && deviceWidth < "767" ? UnderTitleMobile : UnderTitle} alt='' />
                 </div>
                 <div className='d-flex flex-wrap justify-content-center'>
                     <p className={Styles.content}>From simple wellness exams to complex surgeries, our veterinarians and staff are here to serve all your pet&#39;s needs. We provide a full spectrum of on-site services tailored to your pet care and we are confident we can help you.</p>
                 </div>
-                <div className={`${Styles.static}`}>
+                <div className={`${Styles.static} d-none d-lg-block`}>
                     <div className="d-flex justify-content-center align-items-center">
                         {
                             data.map((item, index) => <div className={Styles.boxWrapper} key={index}>
@@ -66,6 +104,19 @@ const HoWeHelp = () => {
                         }
                     </div>
                 </div>
+
+                <div className='d-lg-none'>
+                    <Slider className='asvsd' {...settings}>
+                        {data &&
+                            data.map((item, index) => (
+                                <div key={index} className={Styles.boxWrapper} >
+                                    <Image src={item.icon} alt={item.title} />
+                                    <h3>{item.title}</h3>
+                                </div>
+                            ))}
+                    </Slider>
+                </div>
+
                 <div className={`${Styles.button} text-center`}>
                     <button className="cmn-btn">
                         See All Services
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Head from 'next/head';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 
 function MyApp({ Component, pageProps }) {
