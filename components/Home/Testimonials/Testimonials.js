40b4e89c5edc49cf87e1fa25f5ee4ea0ac79a76d
Jubaear islam Fahim <jubaearislam86664@gmail.com>
2022-10-20 02:52:18 +0600

    Testimonials slider active

--- components/Home/Testimonials/Testimonials.js
+++ components/Home/Testimonials/Testimonials.js
@@ -8,80 +8,124 @@ import profile from "/public/img/Home/Profile_Image/profile-1.svg"
 import profile2 from "/public/img/Home/Profile_Image/profile-2.svg"
 import profile3 from "/public/img/Home/Profile_Image/profile-3.svg"
 import { useSwiper } from 'swiper/react';
-
+import { BsArrowRightCircle,BsArrowLeftCircle } from "react-icons/bs";
 
 
 import { Pagination, Navigation } from "swiper";
 
 
 
-const Testimonials = () => {
-    const swiper = useSwiper();
-    console.log(swiper)
+const Testimonials = () => {   
+    const swiperRef = React.useRef(null);
+
     return (
-        <section id={S.Testimonials}>
-            <button onClick={() => swiper.slideNext()}>Slide to the next slide</button>
+        <section id={S.Testimonials}> 
             <div className={`container-fluid ${S.container}`}>
 
                 <div className={`${S.title} d-flex justify-content-center flex-column`}>
                     <h2 className="title text-center">Our Clients Love Us</h2>
                     <Image src={underTitle} alt="" />
                 </div>
-                <Swiper
-                    slidesPerView={3}
-                    slidesPerGroup={3}
-                    loop={true}
-                    loopFillGroupWithBlank={true}
-                    pagination={{
-                        clickable: true
-                    }}
-                    navigation={true}
-                    modules={[Pagination, Navigation]}
-                    className="mySwiper"
-                >
-                    <SwiperSlide>
-                        <div className={S.SingleSlide}>
-                            <div className={`text-start ${S.comment}`}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s.</div>
-                            <div className="profile d-flex justify-content-start align-items-center">
-                                <div className={S.profile}>
-                                    <Image src={profile} alt="" />
-                                </div>
-                                <div className='d-flex flex-column justify-content-center align-items-center'>
-                                    <span className={`${S.profile_name} d-block`}>Devid Hill</span>
-                                    <span className={`d-block ${S.profile_link}`}>@devid.hill</span>
-                                </div>
-                            </div>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className={S.SingleSlide}>
-                            <div className={`text-start ${S.comment}`}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s.</div>
-                            <div className="profile d-flex justify-content-start align-items-center">
-                                <div className={S.profile}>
-                                    <Image src={profile2} alt="" />
-                                </div>
-                                <div className='d-flex flex-column justify-content-center align-items-center'>
-                                    <span className={`${S.profile_name} d-block`}>Devid Hill</span>
-                                    <span className={`d-block ${S.profile_link}`}>@devid.hill</span>
+                <div className={S.sliderContent}>
+                    <Swiper
+                        ref={swiperRef}
+                        slidesPerView={3}
+                        slidesPerGroup={3}
+                        loop={false}  
+                        loopFillGroupWithBlank={true} 
+                        // navigation={true} 
+                        modules={[Navigation]}
+                        className="mySwiper"
+                        breakpoints={{
+                            340: {
+                                slidesPerView: 1.2,
+                                autoplay: true,
+                            },
+                            768: {
+                                slidesPerView: 2, 
+                            },
+                            1024: {
+                                slidesPerView: 3, 
+                            },
+                        }}
+                    >
+                        <SwiperSlide>
+                            <div className={S.SingleSlide}>
+                                <div className={`text-start ${S.comment}`}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s.</div>
+                                <div className="profile d-flex justify-content-start align-items-center">
+                                    <div className={S.profile}>
+                                        <Image src={profile} alt="" />
+                                    </div>
+                                    <div className='d-flex flex-column justify-content-center align-items-center'>
+                                        <span className={`${S.profile_name} d-block`}>Devid Hill</span>
+                                        <span className={`d-block ${S.profile_link}`}>@devid.hill</span>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className={S.SingleSlide}>
-                            <div className={`text-start ${S.comment}`}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s.</div>
-                            <div className="profile d-flex justify-content-start align-items-center">
-                                <div className={S.profile}>
-                                    <Image src={profile3} alt="" />
-                                </div>
-                                <div className='d-flex flex-column justify-content-center align-items-center'>
-                                    <span className={`${S.profile_name} d-block`}>Devid Hill</span>
-                                    <span className={`d-block ${S.profile_link}`}>@devid.hill</span>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className={S.SingleSlide}>
+                                <div className={`text-start ${S.comment}`}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s.</div>
+                                <div className="profile d-flex justify-content-start align-items-center">
+                                    <div className={S.profile}>
+                                        <Image src={profile2} alt="" />
+                                    </div>
+                                    <div className='d-flex flex-column justify-content-center align-items-center'>
+                                        <span className={`${S.profile_name} d-block`}>Devid Hill</span>
+                                        <span className={`d-block ${S.profile_link}`}>@devid.hill</span>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </SwiperSlide>
-                </Swiper>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className={S.SingleSlide}>
+                                <div className={`text-start ${S.comment}`}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s.</div>
+                                <div className="profile d-flex justify-content-start align-items-center">
+                                    <div className={S.profile}>
+                                        <Image src={profile3} alt="" />
+                                    </div>
+                                    <div className='d-flex flex-column justify-content-center align-items-center'>
+                                        <span className={`${S.profile_name} d-block`}>Devid Hill</span>
+                                        <span className={`d-block ${S.profile_link}`}>@devid.hill</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className={S.SingleSlide}>
+                                <div className={`text-start ${S.comment}`}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s.</div>
+                                <div className="profile d-flex justify-content-start align-items-center">
+                                    <div className={S.profile}>
+                                        <Image src={profile2} alt="" />
+                                    </div>
+                                    <div className='d-flex flex-column justify-content-center align-items-center'>
+                                        <span className={`${S.profile_name} d-block`}>Devid Hill</span>
+                                        <span className={`d-block ${S.profile_link}`}>@devid.hill</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className={S.SingleSlide}>
+                                <div className={`text-start ${S.comment}`}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s.</div>
+                                <div className="profile d-flex justify-content-start align-items-center">
+                                    <div className={S.profile}>
+                                        <Image src={profile3} alt="" />
+                                    </div>
+                                    <div className='d-flex flex-column justify-content-center align-items-center'>
+                                        <span className={`${S.profile_name} d-block`}>Devid Hill</span>
+                                        <span className={`d-block ${S.profile_link}`}>@devid.hill</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                        
+                    </Swiper>
+                    <div className={S.testimonialsBtn}>
+                        <button className={S.prev} onClick={() => swiperRef.current.swiper.slidePrev()} ><BsArrowLeftCircle/></button>
+                        <button className={S.next} onClick={() => swiperRef.current.swiper.slideNext()}><BsArrowRightCircle/></button>
+                    </div>
+                </div>
             </div>
         </section>
     );
