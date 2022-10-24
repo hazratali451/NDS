9290769c93eaa40573b23e7be5fe9a7b7e3a86d1
Md. Hazrat Ali <md.hazratali4510@gmail.com>
2022-10-24 11:54:56 +0600

    progress added

--- pages/_app.js
+++ pages/_app.js
@@ -7,11 +7,18 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import '../styles/swipper.css'
+import NextNProgress from 'nextjs-progressbar';
+
 
 
 function MyApp({ Component, pageProps }) {
 
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextNProgress color="#103559" />
+      <Component {...pageProps} />;
+    </>
+  )
 }
 
 export default MyApp
