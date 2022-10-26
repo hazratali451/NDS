e7b3949d721c56242afaaca83e3ddca4507a8da7
rafuj <raihanrafuj9978@gmail.com>
2022-10-27 01:07:39 +0600

    Initial Commit

--- components/AboutUs/Banner/Banner.js
+++ components/AboutUs/Banner/Banner.js
@@ -1,53 +1,96 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import React, { useEffect, useState } from 'react';
-import S from './Banner.module.css'
-import Dog from "/public/img/About-us/dog.png"
-import underTitle from "/public/img/under-title.svg"
-import underTitleMobile from "/public/img/under-title-mobile.svg"
+import Image from "next/image";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import S from "./Banner.module.css";
+import Dog from "/public/img/About-us/dog.png";
+import underTitleMobile from "/public/img/under-title-mobile.svg";
+import underTitle from "/public/img/under-title.svg";
 
 const Banner = () => {
+	const [deviceWidth, setDeviceWidth] = useState();
 
-    const [deviceWidth, setDeviceWidth] = useState()
+	useEffect(() => {
+		var width = window.innerWidth > 0 ? window.innerWidth : screen.width;
+		setDeviceWidth(width);
+	}, []);
 
-    useEffect(() => {
-        var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-        setDeviceWidth(width)
-    }, [])
-
-    return (
-        <header id={S.banner}>
-            <div className={S.brcm}>
-                <div className="container text-center">
-                    <h3 className={S.brc_title}>About Us</h3>
-                    <ul className='d-flex m-0 p-0 justify-content-center align-items-center'>
-                        <li><Link href={"/"}>Home</Link> </li>
-                        <span>/</span>
-                        <li>About Us</li>
-                    </ul>
-                </div>
-            </div>
-            <div className={S.main}>
-                <div className={`container ${S.container}`}>
-                    <div className={`row align-items-center ${S.row}`}>
-                        <div className="col col-xl-6 col-xxl-7">
-                            <div className={`d-flex flex-column justify-content-center ${S.jhsg}`}>
-                                <h2 className={`title ${S.title}`}>Hi, Let Us Introduce Ourselves</h2>
-                                <div><Image className={S.under_title} src={deviceWidth && deviceWidth < "767" ? underTitleMobile : underTitle} alt="" /></div>
-                            </div>
-                            <p className={`${S.content} mb-0 p-0`}>We are a family-owned company with a mission to provide quality animal health care and service to the community. We are committed to providing our customers with the best care possible which is why we have designed a comfortable, pet-friendly, and calm clinic so that you and your pet can relax. <br /> <br />
-                                Our veterinary team is committed to educating our clients on how to keep your pets healthy year round, with good nutrition and exercise. We stay on top of the latest advances in veterinary technology and above all, remember that all animals and pets need to be treated with loving care in every check-up, procedure, or surgery. <br /><br />
-                                As a full-service animal hospital, we welcome both emergency treatment cases as well as pet patients in need of routine medical, surgical, and dental care. We have many years of experience treating serious conditions and offering regular pet wellness care. We created our clinic with your pet in and mind and that&apos;s why we offer most of the services you pets will need on-site. <br /> <br />
-                                We take pride in serving you. And that&apos;s why In every visit, we go above and beyond to not just assist with your pet&apos;s needs but also to offer a number of resources that enable you to learn about how to take better care of your pets. We make ourselves available to answer any questions you may have. Until you visit us in person, below you can meet our amazing pet-loving team that will be happy to welcome you to our clinic.</p>
-                        </div>
-                        <div className="col col-xl-6 col-xxl-5 text-center">
-                            <Image src={Dog} alt="Dog" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </header >
-    );
+	return (
+		<header id={S.banner}>
+			<div className={S.brcm}>
+				<div className="container text-center">
+					<h3 className={S.brc_title}>About Us</h3>
+					<ul className="d-flex m-0 p-0 justify-content-center align-items-center">
+						<li>
+							<Link href={"/"}>Home</Link>{" "}
+						</li>
+						<span>/</span>
+						<li>About Us</li>
+					</ul>
+				</div>
+			</div>
+			<div className={S.main}>
+				<div className={`container ${S.container}`}>
+					<div className={`row g-4 align-items-center ${S.row}`}>
+						<div className="col col-xl-6 col-xxl-7">
+							<div
+								className={`d-flex flex-column justify-content-center ${S.jhsg}`}
+							>
+								<h2 className={`title ${S.title}`}>
+									Hi, Let Us Introduce Ourselves
+								</h2>
+								<div>
+									<Image
+										className={S.under_title}
+										src={
+											deviceWidth && deviceWidth < "767"
+												? underTitleMobile
+												: underTitle
+										}
+										alt=""
+									/>
+								</div>
+							</div>
+							<p className={`${S.content} mb-0 p-0`}>
+								We are a family-owned company with a mission to provide
+								quality animal health care and service to the community.
+								We are committed to providing our customers with the
+								best care possible which is why we have designed a
+								comfortable, pet-friendly, and calm clinic so that you
+								and your pet can relax. <br /> <br />
+								Our veterinary team is committed to educating our
+								clients on how to keep your pets healthy year round,
+								with good nutrition and exercise. We stay on top of the
+								latest advances in veterinary technology and above all,
+								remember that all animals and pets need to be treated
+								with loving care in every check-up, procedure, or
+								surgery. <br />
+								<br />
+								As a full-service animal hospital, we welcome both
+								emergency treatment cases as well as pet patients in
+								need of routine medical, surgical, and dental care. We
+								have many years of experience treating serious
+								conditions and offering regular pet wellness care. We
+								created our clinic with your pet in and mind and
+								that&apos;s why we offer most of the services you pets
+								will need on-site. <br /> <br />
+								We take pride in serving you. And that&apos;s why In
+								every visit, we go above and beyond to not just assist
+								with your pet&apos;s needs but also to offer a number of
+								resources that enable you to learn about how to take
+								better care of your pets. We make ourselves available to
+								answer any questions you may have. Until you visit us in
+								person, below you can meet our amazing pet-loving team
+								that will be happy to welcome you to our clinic.
+							</p>
+						</div>
+						<div className="col-9 col-sm-6 col-xl-6 col-xxl-5 text-center">
+							<Image src={Dog} alt="Dog" />
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+	);
 };
 
 export default Banner;
--- components/Home/MeetFriends/MeetFriends.js
+++ components/Home/MeetFriends/MeetFriends.js
@@ -1,74 +1,103 @@
-import Image from 'next/image';
-import React, { useState, useCallback, useEffect } from 'react';
-import Styles from './MeetFriends.module.css'
-import underTitle from "/public/img/under-title.svg"
-import underTitleMobile from "/public/img/under-title-mobile.svg"
-import Gallery from "react-photo-gallery";
+import Image from "next/image";
+import React, { useCallback, useEffect, useState } from "react";
 import Carousel, { Modal, ModalGateway } from "react-images";
-import GellaryImage1 from "/public/img/Home/Gellary/Img 3.jpg"
-import GellaryImage2 from "/public/img/Home/Gellary/Img-2.png"
-import GellaryImage3 from "/public/img/Home/Gellary/Img-3.png"
-import GellaryImage4 from "/public/img/Home/Gellary/Img-4.png"
-import GellaryImage5 from "/public/img/Home/Gellary/Img-5.png"
-import GellaryImage6 from "/public/img/Home/Gellary/Img-6.png"
+import Gallery from "react-photo-gallery";
+import Styles from "./MeetFriends.module.css";
+import GellaryImage1 from "/public/img/Home/Gellary/Img 3.jpg";
+import GellaryImage2 from "/public/img/Home/Gellary/Img-2.png";
+import GellaryImage3 from "/public/img/Home/Gellary/Img-3.png";
+import GellaryImage4 from "/public/img/Home/Gellary/Img-4.png";
+import GellaryImage5 from "/public/img/Home/Gellary/Img-5.png";
+import GellaryImage6 from "/public/img/Home/Gellary/Img-6.png";
+import underTitleMobile from "/public/img/under-title-mobile.svg";
+import underTitle from "/public/img/under-title.svg";
 
 const MeetFriends = () => {
-    const data = [GellaryImage1, GellaryImage2, GellaryImage3, GellaryImage4, GellaryImage5, GellaryImage6];
+	const data = [
+		GellaryImage1,
+		GellaryImage2,
+		GellaryImage3,
+		GellaryImage4,
+		GellaryImage5,
+		GellaryImage6,
+	];
 
-    const [currentImage, setCurrentImage] = useState(0);
-    const [viewerIsOpen, setViewerIsOpen] = useState(false);
+	const [currentImage, setCurrentImage] = useState(0);
+	const [viewerIsOpen, setViewerIsOpen] = useState(false);
 
-    const openLightbox = useCallback((event, { photo, index }) => {
-        setCurrentImage(index);
-        setViewerIsOpen(true);
-    }, []);
+	const openLightbox = useCallback((event, { photo, index }) => {
+		setCurrentImage(index);
+		setViewerIsOpen(true);
+	}, []);
 
-    const closeLightbox = () => {
-        setCurrentImage(0);
-        setViewerIsOpen(false);
-    };
+	const closeLightbox = () => {
+		setCurrentImage(0);
+		setViewerIsOpen(false);
+	};
 
+	const [deviceWidth, setDeviceWidth] = useState();
 
+	useEffect(() => {
+		var width = window.innerWidth > 0 ? window.innerWidth : screen.width;
+		setDeviceWidth(width);
+	}, []);
+	return (
+		<section id={Styles.mission}>
+			<div className={`${Styles.container} container-fluid`}>
+				<div
+					className={`${Styles.title} d-flex justify-content-center flex-column`}
+				>
+					<h2 className="title text-center">Meet Our Friends</h2>
+					<Image
+						src={
+							deviceWidth && deviceWidth < "767"
+								? underTitleMobile
+								: underTitle
+						}
+						alt=""
+					/>
+				</div>
+				<div className="d-flex justify-content-center">
+					<p className={Styles.content}>
+						Each visit to our office is unique and we love taking pictures
+						with our furry friends. Feel free to stop by our office and
+						say hello! We would love to build some memories with you too.
+					</p>
+				</div>
 
-    const [deviceWidth, setDeviceWidth] = useState()
-
-
-    useEffect(() => {
-        var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-        setDeviceWidth(width)
-    }, [])
-    return (
-        <section id={Styles.mission}>
-            <div className={`${Styles.container} container-fluid`}>
-                <div className={`${Styles.title} d-flex justify-content-center flex-column`}>
-                    <h2 className="title text-center">Meet Our Friends</h2>
-                    <Image src={deviceWidth && deviceWidth < "767" ? underTitleMobile : underTitle} alt="" />
-                </div>
-                <div className='d-flex justify-content-center'>
-                    <p className={Styles.content}>Each visit to our office is unique and we love taking pictures with our furry friends. Feel free to stop by our office and say hello! We would love to build some memories with you too.</p>
-                </div>
-
-                <div id={Styles.ajksfhdasf}>
-                    <Gallery columns={deviceWidth && deviceWidth < 767 ? 2 : 3} direction={'column'} margin={deviceWidth && deviceWidth < 1100 ? 7 : deviceWidth && deviceWidth < 767 ? 4 : 15} photos={data} onClick={openLightbox} />
-                    <ModalGateway>
-                        {viewerIsOpen ? (
-                            <Modal onClose={closeLightbox}>
-                                <Carousel
-                                    currentIndex={currentImage}
-                                    views={data.map(x => ({
-                                        ...x,
-                                        srcset: x.srcSet,
-                                        caption: x.title
-                                    }))}
-                                />
-                            </Modal>
-                        ) : null}
-                    </ModalGateway>
-                </div>
-
-            </div>
-        </section>
-    );
+				<div id={Styles.ajksfhdasf}>
+					<Gallery
+						columns={deviceWidth && deviceWidth < 767 ? 2 : 3}
+						direction={"column"}
+						margin={
+							deviceWidth && deviceWidth < 1100
+								? 7
+								: deviceWidth && deviceWidth < 767
+								? 4
+								: 15
+						}
+						photos={data}
+						onClick={openLightbox}
+					/>
+					<div className={Styles.firendsOverlay}></div>
+					<ModalGateway>
+						{viewerIsOpen ? (
+							<Modal onClose={closeLightbox}>
+								<Carousel
+									currentIndex={currentImage}
+									views={data.map((x) => ({
+										...x,
+										srcset: x.srcSet,
+										caption: x.title,
+									}))}
+								/>
+							</Modal>
+						) : null}
+					</ModalGateway>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default MeetFriends;
