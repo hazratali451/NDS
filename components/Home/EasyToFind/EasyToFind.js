7c32c9a072a14e2eb4409f8701a11ad40bbc5002
rafuj <raihanrafuj9978@gmail.com>
2022-10-27 07:29:44 +0600

    Initial Commit

--- components/Home/EasyToFind/EasyToFind.js
+++ components/Home/EasyToFind/EasyToFind.js
@@ -1,43 +1,61 @@
-import Image from 'next/image';
-import React, { useEffect, useState } from 'react';
-import S from './EasyToFind.module.css'
-import underTitle from "/public/img/under-title.svg"
-import underTitleMobile from "/public/img/under-title-mobile.svg"
-import Link from 'next/link';
-import DogAndText from "/public/img/Home/Dog-and-ext.svg"
-import Map from "/public/img/Home/Map.svg"
-import Dog from "/public/img/Home/dog-bg.svg"
+import Image from "next/image";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import S from "./EasyToFind.module.css";
+import DogAndText from "/public/img/Home/Dog-and-ext.svg";
+import underTitleMobile from "/public/img/under-title-mobile.svg";
+import underTitle from "/public/img/under-title.svg";
 
 const EasyToFind = () => {
+	const [deviceWidth, setDeviceWidth] = useState();
 
-    const [deviceWidth, setDeviceWidth] = useState()
-
-
-    useEffect(() => {
-        var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-        setDeviceWidth(width)
-    }, [])
-    return (
-        <div id={S.easyToFind}>
-            <div className="container-fluid p-0 overflow-hidden">
-                <div className={`${S.title} d-flex justify-content-center flex-column`}>
-                    <h2 className="title text-center">We Are Easy to Find</h2>
-                    <Image src={deviceWidth && deviceWidth < "767" ? underTitleMobile : underTitle} alt="" />
-                    <p className='text-center'>Our clinic is conveniently located near you</p>
-                </div>
-                <div className={S.wrapper}>
-                    <div className={S.pointer}>
-                        <Link href='/'>
-                            <Image src={DogAndText} alt="" />
-                        </Link>
-                    </div>
-                    <div>
-                        <Image src={Map} alt="" />
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+	useEffect(() => {
+		var width = window.innerWidth > 0 ? window.innerWidth : screen.width;
+		setDeviceWidth(width);
+	}, []);
+	return (
+		<div id={S.easyToFind}>
+			<div className="container-fluid p-0 overflow-hidden">
+				<div
+					className={`${S.title} d-flex justify-content-center flex-column`}
+				>
+					<h2 className="title text-center">We Are Easy to Find</h2>
+					<Image
+						src={
+							deviceWidth && deviceWidth < "767"
+								? underTitleMobile
+								: underTitle
+						}
+						alt=""
+					/>
+					<p className="text-center">
+						Our clinic is conveniently located near you
+					</p>
+				</div>
+				<div className={S.wrapper}>
+					<div className={S.pointer}>
+						<Link href="/">
+							<Image src={DogAndText} alt="" />
+						</Link>
+					</div>
+					<div className={S.pointer}>
+						{/* <Image src={Map} alt="" /> */}
+						<div className={S.gmap_iframe}>
+							<div className="gmap_canvas">
+								<iframe
+									frameborder="0"
+									scrolling="no"
+									marginheight="0"
+									marginwidth="0"
+									src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=University of Oxford&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+								></iframe>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default EasyToFind;
