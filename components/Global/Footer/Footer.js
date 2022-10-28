ea4d9b47e4d27774a7428c14a4918b6ec2a18275
rafuj <raihanrafuj9978@gmail.com>
2022-10-28 21:04:01 +0600

    initial commit

--- components/Global/Footer/Footer.js
+++ components/Global/Footer/Footer.js
@@ -1,156 +1,204 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
-import S from "./Footer.module.css"
-import logo from "/public/img//footer-logo.svg"
-import emailicon from "/public/img/Home/email-icon.svg"
-import facebook from "/public/img/Home/icons/facebook.svg"
-import instagram from "/public/img/Home/icons/instagram.svg"
-import twitter from "/public/img/Home/icons/twitter.svg"
-import Accordion from 'react-bootstrap/Accordion';
-
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Accordion from "react-bootstrap/Accordion";
+import S from "./Footer.module.css";
+import logo from "/public/img//footer-logo.svg";
+import emailicon from "/public/img/Home/email-icon.svg";
+import facebook from "/public/img/Home/icons/facebook.svg";
+import instagram from "/public/img/Home/icons/instagram.svg";
+import twitter from "/public/img/Home/icons/twitter.svg";
 
 const Footer = () => {
+	const footerMenu = {
+		quickLinks: [
+			{
+				title: "Home",
+				link: "/",
+			},
+			{
+				title: "About Us",
+				link: "/about-us",
+			},
+			{
+				title: "Services",
+				link: "/services",
+			},
+			{
+				title: "Resources",
+				link: "/resources",
+			},
+			{
+				title: "Testimonial",
+				link: "/",
+			},
+		],
+		helpAndSupport: [
+			{
+				title: "Help Center",
+				link: "/",
+			},
+			{
+				title: "Privacy Policy",
+				link: "/",
+			},
+			{
+				title: "FAQ",
+				link: "/",
+			},
+			{
+				title: "Terms & Conditions",
+				link: "/",
+			},
+			{
+				title: "Contact",
+				link: "/",
+			},
+		],
+	};
 
-    const footerMenu = {
-        quickLinks: [
-            {
-                title: "Home",
-                link: "/"
-            },
-            {
-                title: "About Us",
-                link: "/"
-            },
-            {
-                title: "Services",
-                link: "/"
-            },
-            {
-                title: "Resources",
-                link: "/"
-            },
-            {
-                title: "Testimonial",
-                link: "/"
-            }
-        ],
-        helpAndSupport: [
-            {
-                title: "Help Center",
-                link: "/"
-            },
-            {
-                title: "Privacy Policy",
-                link: "/"
-            },
-            {
-                title: "FAQ",
-                link: "/"
-            },
-            {
-                title: "Terms & Conditions",
-                link: "/"
-            },
-            {
-                title: "Contact",
-                link: "/"
-            }
-        ]
-    }
-
-
-    return (
-        <div id={S.footer}>
-            <div className={`container px-md-0 ${S.container}`}>
-                <div className="d-lg-flex">
-                    <div className={S.first}>
-                        <div className={S.logo}>
-                            <Image src={logo} alt="Logo" />
-                            <div className={`${S.accordionWrapper} d-lg-none`}>
-                                <Accordion>
-                                    <Accordion.Item className={S.accItem} eventKey="0">
-                                        <Accordion.Header className={`${S.accTitle}`}>Quick Link</Accordion.Header>
-                                        <Accordion.Body className={S.accbody}>
-                                            <ul className='mb-4 p-0'>
-                                                {
-                                                    footerMenu.quickLinks.map((item, index) => <li key={index}><Link href={item.link}>{item.title}</Link></li>)
-                                                }
-                                            </ul>
-                                        </Accordion.Body>
-                                    </Accordion.Item>
-                                    <Accordion.Item className={S.accItem} eventKey="1">
-                                        <Accordion.Header className={`${S.accTitle}`}>Help & Support</Accordion.Header>
-                                        <Accordion.Body className={S.accbody}>
-                                            <ul className='m-0 p-0'>
-                                                {
-                                                    footerMenu.helpAndSupport.map((item, index) => <li key={index}><Link href={item.link}>{item.title}</Link></li>)
-                                                }
-                                            </ul>
-                                        </Accordion.Body>
-                                    </Accordion.Item>
-                                </Accordion>
-                            </div>
-                        </div>
-                        <div className={S.address1}>
-                            <div className={S.address}>Address:</div>
-                            <address className={S.mainAddress}>12991 Potranco Road Suite 108, San Antonio, TX, 78253, United State</address>
-                        </div>
-                        <div>
-                            <div className={S.address}>Office Hours:</div>
-                            <address className={S.mainAddress}>Monday through Friday 8 AM - 5 PM Saturday - Sunday Closed</address>
-                        </div>
-                        <div>
-                            <div className={S.phone}>Phone number: <a href="tel:+(726)888-9024">+(726) 888-9024</a> </div>
-                        </div>
-                    </div>
-                    <div className={`${S.second} d-none d-lg-block`}>
-                        <h3 className={S.quickTitle}>Quick Link</h3>
-                        <ul className='m-0 p-0'>
-                            {
-                                footerMenu.quickLinks.map((item, index) => <li key={index}><Link href={item.link}>{item.title}</Link></li>)
-                            }
-                        </ul>
-                    </div>
-                    <div className={`${S.third} d-none d-lg-block`}>
-                        <h3 className={S.quickTitle}>Help & Support</h3>
-                        <ul className='m-0 p-0'>
-                            {
-                                footerMenu.helpAndSupport.map((item, index) => <li key={index}><Link href={item.link}>{item.title}</Link></li>)
-                            }
-                        </ul>
-                    </div>
-                    <div className={S.fourth}>
-                        <h3 className={S.newsTitle}>
-                            Newsletter
-                        </h3>
-                        <div className={S.newsDesc}>Let`s us keep you inform about special promotions, office events and more</div>
-                        <form action="">
-                            <div className={`${S.form} d-flex align-items-center`}>
-                                <Image height={21.67} width={17.33} src={emailicon} alt='email icon' /> <input type="text" placeholder='Enter your email address' />
-                            </div>
-                            <button className="cmn-btn">
-                                Subscribe
-                            </button>
-                            <div className={S.socialBtns}>
-                                <a href="https://facebook.com" rel="noreferrer" target="_blank">
-                                    <Image src={facebook} alt="facebook icon" />
-                                </a>
-                                <a href="https://instagram.com" rel="noreferrer" target="_blank">
-                                    <Image src={instagram} alt="instagram icon" />
-                                </a>
-                                <a href="https://twitter.com" rel="noreferrer" target="_blank">
-                                    <Image src={twitter} alt="twitter icon" />
-                                </a>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-                <hr />
-                <p className={S.copyrightText}>Copyright © 2021 Pawscare by animalhospital. All Rights Reserved.</p>
-            </div>
-        </div>
-    );
+	return (
+		<div id={S.footer}>
+			<div className={`container px-md-0 ${S.container}`}>
+				<div className="d-lg-flex">
+					<div className={S.first}>
+						<div className={S.logo}>
+							<Image src={logo} alt="Logo" />
+							<div className={`${S.accordionWrapper} d-lg-none`}>
+								<Accordion>
+									<Accordion.Item className={S.accItem} eventKey="0">
+										<Accordion.Header className={`${S.accTitle}`}>
+											Quick Link
+										</Accordion.Header>
+										<Accordion.Body className={S.accbody}>
+											<ul className="mb-4 p-0">
+												{footerMenu.quickLinks.map(
+													(item, index) => (
+														<li key={index}>
+															<Link href={item.link}>
+																{item.title}
+															</Link>
+														</li>
+													)
+												)}
+											</ul>
+										</Accordion.Body>
+									</Accordion.Item>
+									<Accordion.Item className={S.accItem} eventKey="1">
+										<Accordion.Header className={`${S.accTitle}`}>
+											Help & Support
+										</Accordion.Header>
+										<Accordion.Body className={S.accbody}>
+											<ul className="m-0 p-0">
+												{footerMenu.helpAndSupport.map(
+													(item, index) => (
+														<li key={index}>
+															<Link href={item.link}>
+																{item.title}
+															</Link>
+														</li>
+													)
+												)}
+											</ul>
+										</Accordion.Body>
+									</Accordion.Item>
+								</Accordion>
+							</div>
+						</div>
+						<div className={S.address1}>
+							<div className={S.address}>Address:</div>
+							<address className={S.mainAddress}>
+								12991 Potranco Road Suite 108, San Antonio, TX, 78253,
+								United State
+							</address>
+						</div>
+						<div>
+							<div className={S.address}>Office Hours:</div>
+							<address className={S.mainAddress}>
+								Monday through Friday 8 AM - 5 PM Saturday - Sunday
+								Closed
+							</address>
+						</div>
+						<div>
+							<div className={S.phone}>
+								Phone number:{" "}
+								<a href="tel:+(726)888-9024">+(726) 888-9024</a>{" "}
+							</div>
+						</div>
+					</div>
+					<div className={`${S.second} d-none d-lg-block`}>
+						<h3 className={S.quickTitle}>Quick Link</h3>
+						<ul className="m-0 p-0">
+							{footerMenu.quickLinks.map((item, index) => (
+								<li key={index}>
+									<Link href={item.link}>{item.title}</Link>
+								</li>
+							))}
+						</ul>
+					</div>
+					<div className={`${S.third} d-none d-lg-block`}>
+						<h3 className={S.quickTitle}>Help & Support</h3>
+						<ul className="m-0 p-0">
+							{footerMenu.helpAndSupport.map((item, index) => (
+								<li key={index}>
+									<Link href={item.link}>{item.title}</Link>
+								</li>
+							))}
+						</ul>
+					</div>
+					<div className={S.fourth}>
+						<h3 className={S.newsTitle}>Newsletter</h3>
+						<div className={S.newsDesc}>
+							Let`s us keep you inform about special promotions, office
+							events and more
+						</div>
+						<form action="">
+							<div className={`${S.form} d-flex align-items-center`}>
+								<Image
+									height={21.67}
+									width={17.33}
+									src={emailicon}
+									alt="email icon"
+								/>{" "}
+								<input
+									type="text"
+									placeholder="Enter your email address"
+								/>
+							</div>
+							<button className="cmn-btn">Subscribe</button>
+							<div className={S.socialBtns}>
+								<a
+									href="https://facebook.com"
+									rel="noreferrer"
+									target="_blank"
+								>
+									<Image src={facebook} alt="facebook icon" />
+								</a>
+								<a
+									href="https://instagram.com"
+									rel="noreferrer"
+									target="_blank"
+								>
+									<Image src={instagram} alt="instagram icon" />
+								</a>
+								<a
+									href="https://twitter.com"
+									rel="noreferrer"
+									target="_blank"
+								>
+									<Image src={twitter} alt="twitter icon" />
+								</a>
+							</div>
+						</form>
+					</div>
+				</div>
+				<hr />
+				<p className={S.copyrightText}>
+					Copyright © 2021 Pawscare by animalhospital. All Rights Reserved.
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default Footer;
--- components/Home/HoWeHelp/HoWeHelp.js
+++ components/Home/HoWeHelp/HoWeHelp.js
@@ -1,127 +1,131 @@
-import React, { useEffect, useState, Component } from 'react';
-import Icon1 from "/public/img/Home/icons/Icon5.svg"
-import Icon2 from "/public/img/Home/icons/Icon6.svg"
-import Icon3 from "/public/img/Home/icons/Icon7.svg"
-import Icon4 from "/public/img/Home/icons/Icon8.svg"
-import Icon5 from "/public/img/Home/icons/Icon9.svg"
-import Icon6 from "/public/img/Home/icons/Icon10.svg"
-import Styles from './HoWeHelp.module.css'
-import Image from 'next/image';
-import UnderTitle from "/public/img/under-title.svg"
-import UnderTitleMobile from "/public/img/under-title-mobile.svg"
+import Image from "next/image";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
 import Slider from "react-slick";
-
-
+import Styles from "./HoWeHelp.module.css";
+import Icon6 from "/public/img/Home/icons/Icon10.svg";
+import Icon1 from "/public/img/Home/icons/Icon5.svg";
+import Icon2 from "/public/img/Home/icons/Icon6.svg";
+import Icon3 from "/public/img/Home/icons/Icon7.svg";
+import Icon4 from "/public/img/Home/icons/Icon8.svg";
+import Icon5 from "/public/img/Home/icons/Icon9.svg";
+import UnderTitleMobile from "/public/img/under-title-mobile.svg";
+import UnderTitle from "/public/img/under-title.svg";
 
 const HoWeHelp = () => {
+	const [deviceWidth, setDeviceWidth] = useState();
 
-    const [deviceWidth, setDeviceWidth] = useState()
+	useEffect(() => {
+		var width = window.innerWidth > 0 ? window.innerWidth : screen.width;
+		setDeviceWidth(width);
+	}, []);
 
+	const data = [
+		{
+			title: "Urgent Care",
+			icon: Icon1,
+		},
+		{
+			title: "Surgery",
+			icon: Icon2,
+		},
+		{
+			title: "Checkup",
+			icon: Icon3,
+		},
+		{
+			title: "Dental",
+			icon: Icon4,
+		},
+		{
+			title: "Spaying",
+			icon: Icon5,
+		},
+		{
+			title: "Neutering",
+			icon: Icon6,
+		},
+	];
 
-    useEffect(() => {
-        var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-        setDeviceWidth(width)
-    }, [])
+	const settings = {
+		dots: false,
+		infinite: false,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		autoplay: false,
+		arrows: false,
+		swipeToSlide: true,
+		touchMove: true,
+		pauseOnHover: false,
+		responsive: [
+			{
+				breakpoint: 777,
+				settings: {
+					slidesToShow: 3,
+				},
+			},
+			{
+				breakpoint: 500,
+				settings: {
+					slidesToShow: 2,
+				},
+			},
+		],
+	};
 
-    const data = [
-        {
-            title: "Urgent Care",
-            icon: Icon1
-        },
-        {
-            title: "Surgery",
-            icon: Icon2
-        },
-        {
-            title: "Checkup",
-            icon: Icon3
-        },
-        {
-            title: "Dental",
-            icon: Icon4
-        },
-        {
-            title: "Spaying",
-            icon: Icon5
-        },
-        {
-            title: "Neutering",
-            icon: Icon6
-        },
-    ]
+	return (
+		<section id={Styles.HowWeHelp}>
+			<div className={`container-fluid ${Styles.container}`}>
+				<div className="d-flex justify-content-center flex-column align-items-center">
+					<h2 className="title text-center">How We Help</h2>
+					<Image
+						src={
+							deviceWidth && deviceWidth < "767"
+								? UnderTitleMobile
+								: UnderTitle
+						}
+						alt=""
+					/>
+				</div>
+				<div className="d-flex flex-wrap justify-content-center">
+					<p className={Styles.content}>
+						From simple wellness exams to complex surgeries, our
+						veterinarians and staff are here to serve all your pet&#39;s
+						needs. We provide a full spectrum of on-site services tailored
+						to your pet care and we are confident we can help you.
+					</p>
+				</div>
+				<div className={`${Styles.static} d-none d-lg-block`}>
+					<div className="d-flex justify-content-center align-items-center">
+						{data.map((item, index) => (
+							<div className={Styles.boxWrapper} key={index}>
+								<Image src={item.icon} alt={item.title} />
+								<h3>{item.title}</h3>
+							</div>
+						))}
+					</div>
+				</div>
 
+				<div className="d-lg-none">
+					<Slider className="asvsd" {...settings}>
+						{data &&
+							data.map((item, index) => (
+								<div key={index} className={Styles.boxWrapper}>
+									<Image src={item.icon} alt={item.title} />
+									<h3>{item.title}</h3>
+								</div>
+							))}
+					</Slider>
+				</div>
 
-    const settings = {
-        dots: false,
-        infinite: false,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        autoplay: false,
-        arrows: false,
-        swipeToSlide: true,
-        touchMove: true,
-        pauseOnHover: false,
-        responsive: [
-
-            {
-                breakpoint: 777,
-                settings: {
-                    slidesToShow: 3,
-                },
-            },
-            {
-                breakpoint: 500,
-                settings: {
-                    slidesToShow: 2,
-                },
-            },
-        ],
-    };
-
-
-
-
-    return (
-        <section id={Styles.HowWeHelp}>
-            <div className={`container-fluid ${Styles.container}`}>
-                <div className="d-flex justify-content-center flex-column align-items-center">
-                    <h2 className='title text-center'>How We Help</h2>
-                    <Image src={deviceWidth && deviceWidth < "767" ? UnderTitleMobile : UnderTitle} alt='' />
-                </div>
-                <div className='d-flex flex-wrap justify-content-center'>
-                    <p className={Styles.content}>From simple wellness exams to complex surgeries, our veterinarians and staff are here to serve all your pet&#39;s needs. We provide a full spectrum of on-site services tailored to your pet care and we are confident we can help you.</p>
-                </div>
-                <div className={`${Styles.static} d-none d-lg-block`}>
-                    <div className="d-flex justify-content-center align-items-center">
-                        {
-                            data.map((item, index) => <div className={Styles.boxWrapper} key={index}>
-                                <Image src={item.icon} alt={item.title} />
-                                <h3>{item.title}</h3>
-                            </div>)
-                        }
-                    </div>
-                </div>
-
-                <div className='d-lg-none'>
-                    <Slider className='asvsd' {...settings}>
-                        {data &&
-                            data.map((item, index) => (
-                                <div key={index} className={Styles.boxWrapper} >
-                                    <Image src={item.icon} alt={item.title} />
-                                    <h3>{item.title}</h3>
-                                </div>
-                            ))}
-                    </Slider>
-                </div>
-
-                <div className={`${Styles.button} text-center`}>
-                    <button className="cmn-btn">
-                        See All Services
-                    </button>
-                </div>
-            </div>
-        </section>
-    );
+				<div className={`${Styles.button} text-center`}>
+					<Link href="/services">
+						<a className="cmn-btn">See All Services</a>
+					</Link>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default HoWeHelp;
